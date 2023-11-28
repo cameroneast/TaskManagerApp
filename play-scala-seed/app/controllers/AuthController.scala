cdbaac10739828f98b89e2e4a0fd8069267a702f
Mack <clarson9@uncc.edu>
2023-11-27 23:35:12 -0500

    added registering for firebase

--- play-scala-seed/app/controllers/AuthController.scala
+++ play-scala-seed/app/controllers/AuthController.scala
@@ -4,10 +4,12 @@ import javax.inject._
 
 import models.LoginInfo
 import scala.jdk.CollectionConverters._
-
 import play.api._
 import play.api.mvc._
 import play.api.data._
+import models.Manager
+import models.Task
+import scala.collection.mutable.Map
 
 import play.api.data.Forms._
 import play.api.i18n._
@@ -18,7 +20,7 @@ import net.thegreshams.firebase4j.error.FirebaseException;
 import net.thegreshams.firebase4j.error.JacksonUtilityException;
 import net.thegreshams.firebase4j.model.FirebaseResponse;
 import net.thegreshams.firebase4j.service.Firebase;
-
+import scala.collection.mutable.ArrayBuffer
 
 
 class AuthController@Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
@@ -27,7 +29,7 @@ class AuthController@Inject()(cc: MessagesControllerComponents) extends Messages
 
     private val loginURL = routes.AuthController.loginComplete
 
-    def loginEmpty = Action {implicit request: MessagesRequest[AnyContent] => 
+    def loginEmpty() = Action {implicit request: MessagesRequest[AnyContent] => 
         
         Ok(views.html.login(loginForm, loginURL))
     
@@ -38,7 +40,7 @@ class AuthController@Inject()(cc: MessagesControllerComponents) extends Messages
         Ok(views.html.register(registerForm, loginURL))      
     }
 
-    def registerComplete = Action {implicit request: MessagesRequest[AnyContent] =>
+    def registerComplete() = Action {implicit request: MessagesRequest[AnyContent] =>
         val errorFunction =  { (formWithErrors: Form[RegisterData]) => 
             
             BadRequest(views.html.register(formWithErrors, loginURL))
@@ -50,16 +52,21 @@ class AuthController@Inject()(cc: MessagesControllerComponents) extends Messages
             var  hash: HashMap[String, Any] = HashMap("email" -> data.email, "password" -> data.password, "returnSecureToken" -> "true")
             var js = hash.asJava
             firebase.addQuery("key", apiKey)
-            var reponse = firebase.post("signupNewUser", js)
-            var hashRes = reponse.getBody().asScala
+            var reponse = firebase.post( js)
+            /*
+            var hashRes = reponse.getBody().asScala 
             var person = new Person()
-            var hash2: HashMap[String, Any] = HashMap("email" -> hashRes("email"), "userID" -> hashRes("idToken"), "name" -> data.name, "type" -> "Person")
+            var hash2: HashMap[String, Any] = HashMap("email" -> hashRes("email"), "userID" -> hashRes("localId"), "name" -> data.name, "type" -> "Person")
             var hash3: HashMap[String, Any] = HashMap(hashRes("idToken").toString -> hash2)
-            var js2 = hash3.asJava
             var firebase2: Firebase = new Firebase("https://poised-kiln-273723-default-rtdb.firebaseio.com/")
-            firebase2.put(js2);            
-
-            Redirect(routes.AuthController.loginComplete)
+            var js2 = hash2.asJava
+            person.setName(data.name)
+            person.setUserID(hashRes("email").toString)
+            firebase.post(hashRes("localId").toString, js2)
+            */
+            
+            
+            Redirect(routes.AuthController.loginEmpty())
         }
 
         val formValidationResult = registerForm.bindFromRequest()
@@ -75,8 +82,85 @@ class AuthController@Inject()(cc: MessagesControllerComponents) extends Messages
         }
 
         val sucessFunction = {(data: LoginData) =>
+            /*
+            
+            var apiKey = "AIzaSyDwNNjvA03CvvcFHDpTUrYKFBieekCfGI4"
+            var firebase2: Firebase = new Firebase("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword")
+            var hash: HashMap[String, Any] = HashMap("email" -> data.email, "password" -> data.password, "returnSecureToken" -> "true")
+            var js = hash.asJava
+            firebase2.addQuery("key", apiKey)
+            
+            var userRes = firebase2.post(js).getBody().asScala
+            
+            
+            var firebase: Firebase = new Firebase("https://poised-kiln-273723-default-rtdb.firebaseio.com/")
+            var response = firebase.get()
+            var user2 = userRes("localId")
+            var response2 = response.getBody()
+            var responseScala: Map[String, Object] = response2.asScala
+            var hashUser: Map[String, Object] = responseScala
+            if(hashUser("type") == "Person") {
+                var person: Person = new Person()   
+                person.setName(hashUser("name").toString)
+                person.setUserID(userRes("localId").toString)
+                if(hashUser("manager") != null) {
+                    person.setManager(hashUser("manager").toString)
+                }
+                var hashTasks: HashMap[String, Any] = hashUser("tasks")
+                var task = new Task();
+                var array = ArrayBuffer[Any]();
+                if(hashTasks != null) {
+                    for ((k, v) <- hashTasks) {
+                        task = new Task()
+                        array = v.asScala
+                        task.setUserAssigned(array(0).toString)
+                        task.setDescription(array(1).toString)
+                        if(array(2).toString == "true") {
+                            task.setisComplete = true
+                        } else {
+                            task.setisComplete = false
+                        }
+                        person.addTask(task)
+                    }
+                } 
+                Redirect(routes.AuthController.loginComplete)
 
-            Redirect(routes.AuthController.loginComplete)
+                
+            }
+            else {
+                var manager = new Manager()
+                var user = HashMap()
+                manager.setName(hashUser("name").toString)
+                manager.setUserID(hashUser("userID").toString)
+                var hashEmploy = hashUser("people")
+                var person = new Person()
+                for ((k, v: String) <- hashEmploy) {
+                    var user: HashMap[String, Any] 
+                    user = response(v.toString)     
+                    person.setName(user("name").toString)
+                    person.setUserID(user("localId").toString)
+                    if(hashUser("manager") != null) {
+                        person.setManager(user("manager").toString )
+                    }
+                    var hashTasks: HashMap[String, Any] = user("tasks")
+                    var task = new Task();
+                    var array = ArrayBuffer[Any]();
+                    if(hashTasks != null) {
+                        for ((k, v) <- hashTasks) {
+                            task = new Task()
+                            array = v.toScala
+                            task.setUserAssigned(array(0))
+                            task.setDescription(array(1))
+                            task.setisComplete(array(2))
+                            person.addTask()
+                        }
+                    }
+                    manager.addEmployee(person)
+
+                }
+                */
+                Redirect(routes.AuthController.loginComplete())
+            }
         }
 
         val formValidationResult = loginForm.bindFromRequest()
